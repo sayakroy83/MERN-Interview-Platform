59b9a058f44809abb43953504279bdd3fbdd3833
sayak roy <08sayakroy@gmail.com>
2025-11-01 12:12:24 +0530

    initial commit

---
+++ Backend/src/server.js
@@ -0,0 +1,11 @@
+import express from 'express'
+import {ENV} from "./lib/env.js"
+
+
+const app = express()
+
+app.get('/', (req, res)=> {
+    res.status(200).json({msg: "success from backend"})
+})
+
+app.listen(ENV.PORT, ()=> console.log(`server is running on port ${ENV.PORT}`))
