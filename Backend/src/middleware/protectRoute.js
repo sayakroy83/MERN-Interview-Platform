8c6158ba61c3ed4a784df40490b8c418fcfc6639
sayak roy <08sayakroy@gmail.com>
2025-11-05 13:33:21 +0530

    server side auth implemented

---
+++ Backend/src/middleware/protectRoute.js
@@ -0,0 +1,23 @@
+import { requireAuth } from "@clerk/express";
+import User from '../models/User.js'
+
+export const protectRoute = [
+    requireAuth(),
+    async (req, res, next) => {
+        try {
+            const clerkId = req.auth().userId;
+            if(!clerkId) return res.status(401).json({msg: "unauthorized - invalid token"})
+            
+            //find user in DB by clerkId
+            const user = await User.findOne({clerkId})
+            if(!user) return res.status(404).json({msg: "user not found"})
+            
+            //attach user to req
+            req.user = user
+            next()
+        } catch (error) {
+            console.error("error in protectRoute middleware", error)
+            res.status(500).json({msg: "Internal server error"})
+        }
+    }
+]
