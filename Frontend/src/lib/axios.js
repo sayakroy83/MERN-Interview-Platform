d702ae842ca7c905b074a4348a3bf3da2f17a693
sayak roy <08sayakroy@gmail.com>
2026-08-02 09:41:11 +0530

    "updated axios.js"

--- Frontend/src/lib/axios.js
+++ Frontend/src/lib/axios.js
@@ -1,8 +1,10 @@
-import axios from 'axios'
+import axios from "axios";
+
+console.log("API URL =", import.meta.env.VITE_API_URL);
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_API_URL,
-    withCredentials: true
-})
+  baseURL: import.meta.env.VITE_API_URL,
+  withCredentials: true,
+});
 
-export default axiosInstance
+export default axiosInstance;
