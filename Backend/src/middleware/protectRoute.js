2f733ff283a7975d277335b8391f87de1bcb7067
sayak roy <08sayakroy@gmail.com>
2026-08-02 11:32:51 +0530

    "temporary update protect Route.js"

--- Backend/src/middleware/protectRoute.js
+++ Backend/src/middleware/protectRoute.js
@@ -1,23 +1,38 @@
 import { requireAuth } from "@clerk/express";
-import User from '../models/User.js'
+import User from "../models/User.js";
 
 export const protectRoute = [
-    requireAuth(),
-    async (req, res, next) => {
-        try {
-            const clerkId = req.auth().userId;
-            if(!clerkId) return res.status(401).json({msg: "unauthorized - invalid token"})
-            
-            //find user in DB by clerkId
-            const user = await User.findOne({clerkId})
-            if(!user) return res.status(404).json({msg: "user not found"})
-            
-            //attach user to req
-            req.user = user
-            next()
-        } catch (error) {
-            console.error("error in protectRoute middleware", error)
-            res.status(500).json({msg: "Internal server error"})
-        }
+  requireAuth(),
+  async (req, res, next) => {
+    try {
+      console.log("========== AUTH DEBUG ==========");
+      console.log("Headers:");
+      console.log(req.headers);
+
+      console.log("Auth:");
+      console.log(req.auth());
+
+      const clerkId = req.auth().userId;
+
+      console.log("Clerk ID:", clerkId);
+
+      if (!clerkId) {
+        return res.status(401).json({ msg: "No clerkId" });
+      }
+
+      const user = await User.findOne({ clerkId });
+
+      console.log("Mongo User:", user);
+
+      if (!user) {
+        return res.status(404).json({ msg: "User not found" });
+      }
+
+      req.user = user;
+      next();
+    } catch (err) {
+      console.error(err);
+      res.status(500).json({ msg: err.message });
     }
-]
+  },
+];
